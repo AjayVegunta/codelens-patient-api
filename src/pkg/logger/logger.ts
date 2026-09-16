57a667d80211295c3b02f78e8b19b8cb16161cd9
Ajay Vegunta <veguntaajay08@gmail.com>
2026-09-16 14:17:13 +0530

    Enable HTTP runtime logging

--- src/pkg/logger/logger.ts
+++ src/pkg/logger/logger.ts
@@ -1,24 +1,19 @@
-import winston from 'winston';
-import morgan from 'morgan';
+import winston from "winston";
+import morgan from "morgan";
 const { combine, timestamp, json } = winston.format;
 
 export const logger = winston.createLogger({
-    level: 'info',
-    format: combine(
-        timestamp(),
-        json(),
-    ),
-    defaultMeta: { service: 'patient-service' },
-    transports: [
-        new winston.transports.Console(),
-    ],
-})
+  level: "http",
+  format: combine(timestamp(), json()),
+  defaultMeta: { service: "patient-service" },
+  transports: [new winston.transports.Console()],
+});
 
 export const morganMiddleware = morgan(
-    ':method :url :status :res[content-length] - :response-time ms',
-    {
-        stream: {
-            write: (message) => logger.http(message.trim()),
-        },
+  ":method :url :status :res[content-length] - :response-time ms",
+  {
+    stream: {
+      write: (message) => logger.http(message.trim()),
     },
+  },
 );
